45450ef029e80556dc8e28d040b54071caafa2d9
CaioHVectorA <caihebatista@gmail.com>
2023-01-21 18:34:37 -0300

    versao finalizada e funcional

--- tcgapi/src/App.js
+++ tcgapi/src/App.js
@@ -32,24 +32,20 @@ background: #d9d9d9;
 position: relative;
 bottom: 47px;
 left: 89%;
-border-radius: 5px;
+border-radius: 0px 5px 5px 0px;
 &:hover {
 background: #e7e7e7;
 cursor: pointer;
 
 }
 `
-// const ModalSearch = () => {
-//   return (
-//   <div style={{width: '100vw',display: 'flex',justifyContent: 'center',alignItems: 'center',height: '100vh',backgroundColor: 'rgba(0,0,0,.6)',position: 'absolute',top: '0%',left: '0px'}}>
-//     <div style={{width: '400px',height: '200px',backgroundColor: '#e7e7e7',borderRadius: '25px',paddingTop: '32px'}}>
-    
-//     <Search></Search>
-//     </div>
-//   </div>
-
-//   )
-// }
+const Detail = styled.div`
+border-radius: 5px;
+background-color: rgba(255,255,255,0.2);
+height: 4px;
+width: 80%;
+margin: 0 auto;
+`
 
 function App() {
   const [Array,setArray] = useState([])
@@ -84,10 +80,13 @@ function App() {
   function SearchModal() {
   const ArrayFiltrada = Array.filter(item => item.name.toUpperCase().startsWith(inputValue.toUpperCase()))
   setBusca(ArrayFiltrada)
-  }
+}
+useEffect(() => {
+  console.log(Busca)
+},[Busca])
   return (
     <div className="App">
-      {!Filtro && <div>
+      {!Filtro &&  !Busca && <div>
       <h1>Bem vindo ao Centro TCG!</h1>
       <p>Todas as suas informações sobre as cartas aqui.</p>
       <h2>O que deseja?</h2>
@@ -98,16 +97,22 @@ function App() {
           <svg style={{scale: '0.7'}} xmlns="http://www.w3.org/2000/svg" fill="#000" height="48" width="48"><path d="M39.8 41.95 26.65 28.8q-1.5 1.3-3.5 2.025-2 .725-4.25.725-5.4 0-9.15-3.75T6 18.75q0-5.3 3.75-9.05 3.75-3.75 9.1-3.75 5.3 0 9.025 3.75 3.725 3.75 3.725 9.05 0 2.15-.7 4.15-.7 2-2.1 3.75L42 39.75Zm-20.95-13.4q4.05 0 6.9-2.875Q28.6 22.8 28.6 18.75t-2.85-6.925Q22.9 8.95 18.85 8.95q-4.1 0-6.975 2.875T9 18.75q0 4.05 2.875 6.925t6.975 2.875Z"/></svg>
         </Division>
         
-      <ButtonN onClick={() => {setFiltro('All')}}>Mostrar todos</ButtonN>
+        <Detail></Detail>
+      {!Busca && <div style={{display: 'grid'}}>
+        <p>Sem uma carta em mente?</p>
+        <ButtonN onClick={() => {setFiltro('All')}}>Mostrar todos</ButtonN>
+      </div>
+      }
       </div>
         </div>}
+      {Busca && <ButtonN onClick={() => {setBusca(null)}}>Voltar a página inicial</ButtonN>}
      {Array.length === 4945 && Filtro === 'All' && <div>
-      <h2>Total: {Array.length} Cartas.</h2>
+      <h2>Mostrando todas as {Array.length} Cartas.</h2>
       <ButtonN onClick={() => setFiltro(null)}>Voltar ao normal</ButtonN>
       {Array.map(item => (
         <div key={item.id}>
         <h1 style={{fontFamily: 'Arial'}}>{item.name}</h1>
-        <img alt='CARREGANDO-IMAGEM' style={{height: '400px',width: '280px',backgroundColor: '#d9d9d9'}} src={`${item.image}/low.png`} />
+        <img alt='CARREGANDO-IMAGEM' style={{height: '400px',width: '280px',backgroundColor: '#161616',borderRadius: '20px'}} src={`${item.image}/low.png`} />
         </div>
       ))}
       </div>}
@@ -119,6 +124,7 @@ function App() {
                   </div>
         ))}
         </div>}
+        {Busca !== null && Busca.length === 0 && <h1>Nenhuma carta foi encontrada.Use palavras chave mais específicas!</h1>}
       {Array.length !== 4945 && Filtro === 'All' && <h1>Carregando...</h1>}
     </div>
   );
